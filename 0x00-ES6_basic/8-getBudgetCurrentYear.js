b2f1f8795afcc7b3eb38fbfa3c3739082091b44a
nessy254 <mukaminessy9@gmail.com>
2024-06-27 18:23:33 +0300

    object properties ES6

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,10 +4,12 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
 
-	return {
-		[`income-${getCurrentYear()}`] : income,
-		[`gdp-${getCurrentYear()}`] : gdp,
-		[`capita-${getCurrentYear}`] : capita
-	};
+  return {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita
+  };
 }
+
