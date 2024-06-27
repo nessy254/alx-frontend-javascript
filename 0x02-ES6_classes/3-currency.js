7093c8c649af64b20e53a9cd774feb08edab0997
nessy254 <mukaminessy9@gmail.com>
2024-06-28 00:09:12 +0300

    class currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,22 +1,22 @@
-export default class Currency{
-  constructor(code, name){
+export default class Currency {
+  constructor(code, name) {
     this._code = code;
     this._name = name;
   }
-  get code(){
+  get code() {
     return this._code;
-  };
+  }
 
-  get name(){
+  get name() {
     this._name = name
   }
-  set code(code){
+  set code(code) {
     this._code = code;
-  };
+  }
 
-  set name(){
+  set name() {
     this._name = name;
-  };
+  }
 
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
