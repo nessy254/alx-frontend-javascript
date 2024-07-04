6c8694f74a82176d544cdc63b1f03a69df1c345b
nessy254 <mukaminessy9@gmail.com>
2024-07-04 16:39:00 +0300

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
-export function getListStudents() {
+export default function getListStudents() {
   const myArray = [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
-    { id: 2, firstName: "James", location: "Columbia" },
-    { id: 3, firstName: "Serena", location: "San Francisco" }
-  ];
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 3, firstName: 'Serena', location: 'San Francisco' }
+  ]
 
   return myArray;
 }
