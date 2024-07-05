04b37fc204a9b3f841ffdfbf818c8ae0d3fb11f5
nessy254 <mukaminessy9@gmail.com>
2024-07-05 13:44:38 +0300

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,8 @@
 export default function updateStudentGradeByCity(myArray, city, newGrades) {
   return myArray
-    .filter((student) => student.location === city);
-    .map((student) => student.grade === newGrades);
+    .filter((student) => student.location === city)
+    .map((student) => {
+      let grade = newGrades.find(g => g.studentId === student.id);
+      return { ...student, grade: (grade && grade.grade) || 'N/A' };
+    });
 }
