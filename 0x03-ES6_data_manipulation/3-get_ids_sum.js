9377a0102481d2a58f420f36c5c97e76f11b35e8
nessy254 <mukaminessy9@gmail.com>
2024-07-05 13:02:25 +0300

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(myArray) {
-  myArray.reduce((total, student) => total + student.id, 0)
+  return myArray.reduce((total, student) => total + student.id, 0)
 }
