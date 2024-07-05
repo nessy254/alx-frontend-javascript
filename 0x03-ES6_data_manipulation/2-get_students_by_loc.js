518c4460c6a70ac8a2723eafb40b62241b0be229
nessy254 <mukaminessy9@gmail.com>
2024-07-05 12:43:41 +0300

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(students,city) {
-  return students.filter((student) => student.location === city);
+export default function getStudentsByLocation(myArray, city) {
+  return myArray.filter((student) => student.location === city);
 }
