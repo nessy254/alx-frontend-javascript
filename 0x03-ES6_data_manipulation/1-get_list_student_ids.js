524db179d2300cf4d18c43824aa7d808f3834ddd
nessy254 <mukaminessy9@gmail.com>
2024-07-05 12:48:15 +0300

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(myArray) {
-  if(!Array.isArray(myArray)) {
+  if (!Array.isArray(myArray)) {
     return [];
   }
-  return myArray.map(student => student.id);
+  return myArray.map((student) => student.id);
 }
